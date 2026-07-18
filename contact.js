3bf8987f3b5cc5833ea71ae8705a411bc9e5917c
kamalesh-slay <kamalesh3258@gmail.com>
2026-07-18 16:51:13 +0530

    Add files via upload

---
+++ contact.js
@@ -0,0 +1,10 @@
+var popup = document.querySelector("#complete")
+var button= document.querySelector("#submit")
+function show(){
+    popup.style.display="block"
+
+    setTimeout(() => {
+        popup.style.display="none"
+    }, 3000);
+}
+    
